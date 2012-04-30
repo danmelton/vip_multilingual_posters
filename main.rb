7668476d63b5752c989521d4c8c564933f009ebc
Dan Melton <melton.dan@gmail.com>
2012-04-30 08:13:47 -0700

    added sinatra

---
+++ main.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+  "Step 1: Enter an Address"
+end
+
+get '/step2' do
+  "Step 2: Select a Language"
+end
+
+get '/step3' do
+  "Step 3: Download Posters"
+end
