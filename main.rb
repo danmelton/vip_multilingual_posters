2f956a6d78b089c83984ccf2e5fe72a74fd94d78
Dan Melton <melton.dan@gmail.com>
2012-04-30 11:36:06 -0700

    adding vip connector

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require 'geocoder'
 require 'json'
 require 'sinatra'
 
-API_KEY = ENV['VIP_KEY']
+API_KEY = ENV['VIP_KEY'] || 'FD854C9B-AB50-4652-B5E9-616BA87E165D'
 
 get '/' do
   "Step 1: Enter an Address"
@@ -18,37 +18,37 @@ get '/step2' do
   session[:time1] = params[:time2]
   session[:time2] = params[:time2]
   address = Geocoder.search(params[:address])
-
-  address.first.city
-  google_map_image = 
+  location = vip_object(address.first)
+  session[:polling_name] = location["Address"]["LocationName"]
+  session[:polling_ccoordinates] = location["Address"]["Lat"].to_s + "," + location["Address"]["Lon"].to_s
+  session[:polling_address1] = location["Address"]["Line1"]
+  session[:polling_address2] = location["Address"]["City"] + ", " +location["Address"]["State"] + " " + location["Address"]["Zip"]   
   erb :step2  
 end
 
 get '/step3' do
-  "Step 3: Download Posters"
+  
+  session[:languages] = params[:language]
+  
   erb :step3
 end
 
-def vip(geocoder_object)
-  street_number = geocoder_object.address_components_of_type(:street_number).first["long_name"]
-  street = geocoder_object.address_components_of_type(:route).first["long_name"]
-  city = geocoder_object.city
-  state = geocoder_object.state_code
-  zip = geocoder_object.postal_code 
-  url = URI('http://api.votinginfoproject.org/vip/3.0/GetPollingLocations2')
-  params = {
-    "house" => street_number,
-    "street" => street,
-    "city" => city,
-    "state"=> state,
-    "zip"=> zip,
-    "format"=> "json",
-    "$expand"=> "Election/State/ElectionAdministration,Locations/PollingLocation/Address,Locations/SourceStreetSegment/NonHouseAddress",
-    "key" => API_KEY,
-    "onlyUpcoming"=>"false"
-  }
-  url.query(params)
+get '/download' do
+  size = params[:size]
 end
+
+def vip_object(geocoder_object)
+  street_number = CGI::escape(geocoder_object.address_components_of_type(:street_number).first["long_name"])
+  street = CGI::escape(geocoder_object.address_components_of_type(:route).first["long_name"])
+  city = CGI::escape(geocoder_object.city)
+  state = CGI::escape(geocoder_object.state_code)
+  zip = CGI::escape(geocoder_object.postal_code)   
+  url = "http://api.votinginfoproject.org/vip/3.0/GetPollingLocations2?house="+street_number+"&street='"+street+"'&city='"+city+"'&state='"+state+"'&zip='"+zip+"'&%24format=json&$expand=Election/State/ElectionAdministration,Locations/PollingLocation/Address,Locations/SourceStreetSegment/NonHouseAddress&onlyUpcoming=false&key="+API_KEY
+  doc = open(url).read
+  json_obj = JSON.parse(doc.gsub("\r\n", ""))
+  json_obj["d"]["results"].first["Locations"]["results"].first["PollingLocation"]
+end
+
 
 def google_map(latlon)
   "http://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=16&size=512x512&maptype=roadmap&markers=icon:http://chart.apis.google.com/chart?chst=d_bubble_text_small%26chld=bb%257CVote Here!%257CFFFF88%257C000000|"+latlon+"&sensor=false"
