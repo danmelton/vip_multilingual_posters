085eb2df16eb429a83a53acc912f4e4974c8b432
Dan Melton <melton.dan@gmail.com>
2012-04-30 08:42:51 -0700

    adding file structure, images and css

--- main.rb
+++ main.rb
@@ -3,12 +3,15 @@ require 'sinatra'
 
 get '/' do
   "Step 1: Enter an Address"
+  erb :step1
 end
 
 get '/step2' do
   "Step 2: Select a Language"
+  erb :step2  
 end
 
 get '/step3' do
   "Step 3: Download Posters"
+  erb :step3
 end
